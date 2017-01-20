f4f3410ed03bebbac3af8252c187cb31dfe45e1c
7szczepanski <7szczepanski@gmail.com>
2017-01-20 15:47:04 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,21 @@
 #include <SFML\Graphics.hpp>
 #include <iostream>
+#include <time.h>
+#include <random>
+
 using namespace std;
 
 
-int const cols = 10;
-int const rows = 10;
-//double grid[cols][rows];
-// Just for loggin
-int openSet[];
-int closedSet[];
+int const cols = 20;
+int const rows = 20;
 int width = 800;
 int height = 800;
 double sizx = (width / cols);
 double sizy = (height / rows);
+
 sf::RenderWindow window(sf::VideoMode(width, height), "A-STAR");
-int start[cols][rows];
-int end[cols][rows];
+
+
 
 
 class Spot {
@@ -23,15 +23,22 @@ public:
 
 	void drawRect() {
 		sf::RectangleShape rect(sf::Vector2f(sizx, sizy));
-		//sf::CircleShape rect(50.f);
 		rect.setPosition((pos_x), (pos_y));
 		rect.setOutlineThickness(0.5);
 		rect.setOutlineColor(sf::Color::Black);
-		if (start == true) { rect.setFillColor(sf::Color::Blue); }
+		//if (start == true) { rect.setFillColor(sf::Color::Blue); }
 		if (end == true) { rect.setFillColor(sf::Color::Red); }
+		if (open == true) { rect.setFillColor(sf::Color::Green); }
+		if (closed == true) { rect.setFillColor(sf::Color::Yellow); }
+
 		window.draw(rect);
 	}
-	
+	void isClosed(bool is) {
+		closed = is;
+	}
+	void isOpen(bool is) {
+		open = is;
+	}
 	void isStart(bool is) {
 		start = is;
 	}
@@ -45,36 +52,63 @@ public:
 	void setPos_y(int jterator) {
 		pos_y = sizy*jterator;
 	}
-
+	
+	bool getIsOpen() {
+		return open;
+	}
+	bool getIsClosed() {
+		return closed;
+	}
 	int getPos_x() {
 		//cout << "pos_x = " << pos_x << endl;
 		return pos_x;
 	}
-private:
-	int pos_x;
-	int pos_y;
+	int getPos_y() { return pos_y; }
+	
 	int f = 0;
 	int g = 0;
 	int h = 0;
+private:
+	int pos_x;
+	int pos_y;
+
 	bool start = false;
 	bool end = false;
+	bool open = false;
+	bool closed = false;
 
 };
+
+
 int main()
 {
+	srand(time(NULL));
 	Spot grid[cols][rows];
+	int startX = 0;
+	int startY = 0;
+	int endX = cols-1;
+	int endY = rows-1;
+	bool testisobstacle = false;
+	bool testisobstacleE = false;
+	vector<Spot> openSet;
+	vector<Spot> closedSet;
+
+	grid[startX][startY].setPos_x(startX);
+	grid[startX][startY].setPos_y(startY);
+	grid[startX][startY].isStart(true);
+
+	grid[endX][endY].setPos_x(endX);
+	grid[endX][endY].setPos_y(endY);
+	grid[endX][endY].isEnd(true);
+
+
 
 	
-	sf::CircleShape shape(100.f);
-	sf::RectangleShape rect(sf::Vector2f(sizx, sizy));
-	sf::RectangleShape rect2(sf::Vector2f(sizx, sizy));
-	//rect.setPosition((sizx* 7)-5, (sizy * 0));
-	//rect2.setPosition((sizx * 6)-10, (sizy * 0));
-	//rect.setOutlineColor(sf::Color::Black);
-	//rect2.setOutlineColor(sf::Color::Black);
-	//rect2.setFillColor(sf::Color::Green);
+	openSet.push_back(grid[startX][startY]);
 
 
+	
+	/*---------------------------------------------------------------   DRAW  --------------------------------------------------------*/
 	while (window.isOpen())
 	{
 		sf::Event evnt;
@@ -84,23 +118,47 @@ int main()
 				window.close();
 		}
 
+		for (unsigned int i = 0; i < closedSet.size(); i++)
+		{
+			closedSet[i].isClosed(true);
+		}
+		for (unsigned int i = 0; i < openSet.size(); i++)
+		{
+			openSet[i].isOpen(true);
+		}
+
+		if (openSet.size() > 0) {
+			int winner = 0;
+			for (int i = 0; i < openSet.size(); i++) {
+				if (openSet[i].f < openSet[winner].f)
+				{
+					winner = i;
+				}
+			}
+
+			auto current = openSet[winner];
+
+			if (current.getPos_x() == grid[endX][endY].getPos_x() && current.getPos_y() == grid[endX][endY].getPos_y()) {
+				cout << "Here it is!" << endl;
+			}
+			closedSet.push_back(current);
+			openSet.erase(openSet.begin()+winner);
+			
+	}
+		else {
+			// no solution
+		}
+
 		for (int i = 0; i < cols; i++)
 		{
 			for (int j = 0; j < rows; j++)
 			{
-
 				grid[i][j].setPos_x(i);
 				grid[i][j].setPos_y(j);
 				grid[i][j].drawRect();
-				if (i == 0 && j == 0) { grid[i][j].isStart(true); }
-				if (i == cols-1 && j == rows-1) { grid[i][j].isEnd(true); }
 			}
-		}
 
-		//window.clear();
-		//	window.draw(shape);
-		//window.draw(rect);
-		//window.draw(rect2);
+		}
 		window.display();
 	}
 
